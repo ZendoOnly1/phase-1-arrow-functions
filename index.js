7dece87ad472245d59985cadaa0054b9473ee28c
Jaydenzel <jaydenzel.gitonga@student.moringaschool.com>
2025-03-25 16:41:14 +0300

    Implemented methods for lab instructions

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function() {
+    return 2000 / 100;
+};
+const square = (x) => x * x;
+const foo = function() {
+    return 'bar';
+};
+const add = (param1, param2) => param1 + param2;
+const nums = [1, 2, 3];
+const squares = nums.map(num => num ** 2); 
+console.log(squares); // [1, 4, 9]
